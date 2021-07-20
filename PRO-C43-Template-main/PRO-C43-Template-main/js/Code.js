95191395a9695ea037eb463b89240451625da9a7
Mridul2201 <72970571+Mridul2201@users.noreply.github.com>
2021-07-20 20:48:33 +0530

    Add files via upload

---
+++ PRO-C43-Template-main/PRO-C43-Template-main/js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+    
+function clues() {
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("green")    
+    textSize(15)
+    text(" T O N I N U C F " , 690 , 160 )
+    text("Hint: Another word like procedure !!", 690 , 180)
+
+    fill("white")
+    text("E A S B A D T A" , 300 , 320 )
+    text("Hint: A place where all the information is stored" , 300, 300)
+
+
+
+}
